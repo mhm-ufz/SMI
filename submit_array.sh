3be03fd1185447ad0c490da716dfa90c14fa00b7
Stephan Thober <stephan.thober@ufz.de>
2014-07-09 11:13:00 +0000

    2014/NMME-Forecast/SMI: adapted submit script
    git-svn-id: https://svn.ufz.de/svn/stohydrol/progs/SMI@2914 5f28456f-7765-ee9a-d85f-fbc0b3c57123

--- submit_array.sh
+++ submit_array.sh
@@ -9,10 +9,12 @@
 # #$ -t 1-2591
 # # submission for all model members, months, and years
 # #$ -t 1-32723
-# submission for all model members, months, and years
+# # submission for Grand Ensemble, or Ensemble for one model
 #$ -t 1-323
-#$ -l h_rt=2:00:00                    # Request resource: hard run time hours:minutes:seconds
-#$ -l h_vmem=6G                       # Request resource: memory requirements/per slot
+# submission for Statistical model
+# #$ -t 1-3239
+#$ -l h_rt=0:10:00                    # Request resource: hard run time hours:minutes:seconds
+#$ -l h_vmem=1G                       # Request resource: memory requirements/per slot
 #$ -l centos6=true                    # run on eve idiv  
 #$ -cwd                               # Change into directory where you wrote qsub
 #----------------------------------------------------
